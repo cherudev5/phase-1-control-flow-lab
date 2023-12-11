2245934fa9c5170b859d1f1523754a9078ff57bf
David <cheruiyotscripts@gmail.com>
2023-12-11 16:17:27 +0300

    completed assingment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(feet){
+      let result;
+    if (feet <= 400) {
+      return'This one is on me!';
+    } else if (feet > 2000 && feet < 2500) {
+      return'I will gladly take your thirty bucks.';
+    } else if (feet >= 2500) {
+      return'No can do.';
+    }
+    return result;
+  }
+  
+function ternaryCheckCity(city) {
+    return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip) {
+    let response;
+    switch (tip) {
+      case 'generous':
+        return'Thank you so much.';
+        break;
+      case 'not as generous':
+        return'Thank you.';
+        break;
+      default:
+        return'Bye.';
+        break;
+    }
+    return response;
+  }
+  
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
